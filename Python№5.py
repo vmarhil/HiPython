2954e72c743b3663319badd699f040c1e059c9b4
vMarhil <marxill@mail.ru>
2022-04-17 17:54:36 +0500

    Python№5.py

---
+++ Python№5.py
@@ -0,0 +1,9 @@
+# Дано число. Проверить кратно ли оно 5 и 10 или 15 но не 30
+import os
+os.system('cls')
+
+n=60
+if (n%5==0 and n%10==0) or (n%15==0 and n%30!=0):
+    print(True)
+else:
+    print(False)
