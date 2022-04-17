385177e35b84afe6d75bfb52eaaed8be8078a35d
vMarhil <marxill@mail.ru>
2022-04-17 17:53:45 +0500

    Python№2.py

---
+++ Python№2.py
@@ -0,0 +1,24 @@
+
+# Найти максимальное из пяти чисел
+
+import os
+os.system('cls')
+
+a = 10
+b = 8
+c = 33
+d = 5
+e = 6
+max = a
+if (a > max): 
+    max = a
+if (b > max): 
+    max = b
+if (c > max): 
+    max = c
+if (d > max): 
+    max = d
+if (e > max): 
+    max = e
+
+print(max)
